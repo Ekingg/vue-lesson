9beb9992af860c0bbcdeaa5e3a90d77dbf25e415
panda <SmelayaPanda@mail.ru>
2017-12-09 20:58:25 +0700

    lesson_3

---
+++ lesson_3/app.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: '#app',
+    data: {},
+    methods: {
+        logName: function () {
+            console.log('name typed')
+        },
+        logAge: function () {
+            console.log('age typed')
+        }
+    }
+});
