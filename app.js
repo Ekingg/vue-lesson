1d26de7771e6615cf0e583d9be66bd40f94ab85f
panda <SmelayaPanda@mail.ru>
2017-12-09 15:00:14 +0700

    1

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ new Vue({
     el: '#vue-app',
     data: {
         name: 'Alexey',
-        job: 'Developer'
+        job: 'Developer',
+        website: 'https://vk.com/im?sel=63014461',
+        websiteTag: '<a href="https://vk.com/im?sel=63014461"> my site</a>'
     },
     methods: {
-        greet: function () {
-            return 'Good Morning'
+        greet: function (time) {
+            return 'Good ' + time +' ' + this.name;
         }
     }
 });
