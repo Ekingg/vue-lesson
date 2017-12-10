6265c74fc1b374ba9e35d90975b2f6d1035662a0
panda <SmelayaPanda@mail.ru>
2017-12-10 11:26:04 +0700

    lesson_8

---
+++ lesson_8/app.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: '#app',
+    data: {
+        characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
+        ninjas: [
+            {name: 'Rui', age: 10},
+            {name: 'Yup', age: 11},
+            {name: 'Koj', age: 13}
+        ]
+    },
+    methods: {},
+    computed: {}
+});
