4e6b16bef8c74304490180eee74c46bfbb5cda18
panda <SmelayaPanda@mail.ru>
2017-12-17 20:01:52 +0700

    lesson 31 blog (v-model for checkbox, lazy, ...)

--- lesson_21_and_above_in_branches/src/App.vue
+++ lesson_21_and_above_in_branches/src/App.vue
@@ -1,34 +1,27 @@
-<!-- Root Component -->
 <template>
   <div>
-    <!--Use keep alive to save preset values-->
-    <keep-alive>
-      <component v-bind:is="component"></component>
-    </keep-alive>
-    <br>
-    <button v-on:click="component='form-one'">show form one</button>
-    <button v-on:click="component='form-two'">show from two</button>
+    <add-blog></add-blog>
   </div>
 </template>
 
-
 <script>
-  import formOne from './components/formOne'
-  import formTwo from './components/formTwo'
+  // Imports
+  import addBlog from './components/addBlog.vue';
 
   export default {
     components: {
-      'form-one': formOne,
-      'form-two': formTwo
+      'add-blog': addBlog
     },
     data() {
-      return {
-        component: 'form-one'
-      }
-    }
+      return {}
+    },
+    methods: {}
   }
 </script>
 
-
 <style>
+  body {
+    margin: 0;
+    font-family: 'Nunito SemiBold';
+  }
 </style>
