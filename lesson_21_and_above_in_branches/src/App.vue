da90f15f50426465612346e817446ff4105d9bfb
panda <SmelayaPanda@mail.ru>
2017-12-21 00:14:41 +0700

    lesson 41 route links

--- lesson_21_and_above_in_branches/src/App.vue
+++ lesson_21_and_above_in_branches/src/App.vue
@@ -1,15 +1,33 @@
 <template>
   <div>
     <h1>Main page</h1>
+    <nav>
+      <ul>
+        <!-- This way will reload page -->
+        <!--<li><a href="/">Home</a></li>-->
+        <!--<li><a href="/one">Page one</a></li>-->
+        <!--<li><a href="/two">Page two</a></li>-->
+
+        <li>
+          <router-link to="/" exact>Home</router-link>
+        </li>
+        <li>
+          <router-link to="/one" exact>Page one</router-link>
+        </li>
+        <li>
+          <router-link to="/two" exact>Page two</router-link>
+        </li>
+        <!-- exact for selector .router-link-active:
+             exactly for this url without any parent-->
+      </ul>
+    </nav>
     <router-view></router-view>
-    <!--<page-one></page-one>-->
-    <!--<page-two></page-two>-->
   </div>
 </template>
 
 <script>
-import pageOne from './components/page-1'
-import pageTwo from './components/page-2'
+  import pageOne from './components/page-1'
+  import pageTwo from './components/page-2'
 
   export default {
     components: {
@@ -30,5 +48,10 @@ import pageTwo from './components/page-2'
 
   input {
     width: 435px;
+  }
+
+  .router-link-active {
+    background-color: cornflowerblue;
+    border-radius: 3px;
   }
 </style>
